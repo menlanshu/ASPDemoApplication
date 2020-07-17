11a44a7ba784d45218aed0b44eaf0236800e6160
fulong <long_fu@amat.com>
2020-07-17 17:19:40 +0800

    BootStrap demo code add

---
+++ MVCDemo/Controllers/PeopleController.cs
@@ -0,0 +1,29 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+
+            people.Add(new PersonModel { FirstName = "Fu", LastName = "Long", Age = 20 });
+            people.Add(new PersonModel { FirstName = "Kuang", LastName = "Dali", Age = 18 });
+            people.Add(new PersonModel { FirstName = "Hua", LastName = "Tingting", Age = 25 });
+
+            return View(people);
+        }
+    }
+}
